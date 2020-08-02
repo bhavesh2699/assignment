da6ca75aee7be8da86c222da7cc031d01ac40344
Bhavesh Solanki <41828589+bhavesh2699@users.noreply.github.com>
2020-08-02 18:34:27 +0530

    DOM_Exercise

---
+++ assignment 4/ExerciseEvents.js
@@ -0,0 +1,73 @@
+/*
+  Exercises 01
+  ------------
+  Add a label to each of the input fields: username, password, confirm password
+  
+*/
+function addLabel() { 
+    document.getElementById('uname').innerHTML = 'Username'; 
+    document.getElementById('pass').innerHTML = 'Password'; 
+    document.getElementById('cnf_pass').innerHTML = 'Confirm Password'; 
+} 
+addLabel();
+
+/*
+  Exercise 02
+  -----------
+  Add a required validation to each input that shows an error message next to the entry if it does not have any text entered.
+*/
+document.getElementById("mySubmit").disabled = true;
+
+function chk(){
+    const uname = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
+    const cnf_pass = document.getElementById('confirmPassword').value;
+
+    if(uname === ''){
+        event.preventDefault();
+        document.getElementById('chk_uname').innerHTML = 'Enter Username !!!';
+    }
+    else{
+        document.getElementById('chk_uname').innerHTML = ''; 
+    }
+
+    if(pass === ''){
+        event.preventDefault();
+        document.getElementById('chk_pass').innerHTML = 'Enter Password !!!'; 
+    }
+    else{
+        document.getElementById('chk_pass').innerHTML = ''; 
+    }
+
+    if(cnf_pass === ''){
+        event.preventDefault();
+        document.getElementById('chk_cnf_pass').innerHTML = 'Confirm Password !!!'; 
+    }
+
+    /*
+    Exercise 03
+    -----------
+    Add a further validation to check if the user input in the password and confirm password inputs match.  Show an error message if they do not.
+    */
+    else if(pass != cnf_pass){
+        event.preventDefault();
+        document.getElementById('chk_cnf_pass').innerHTML = 'Password does not match !!!'; 
+    }
+    /*
+    Exercise 04
+    -----------
+    Ensure the ‘Register’ button is disabled until the user has entered valid data into all the input fields.  Once they have, the registration button should then be enabled.
+    */
+    else{
+        document.getElementById('chk_cnf_pass').innerHTML = ''; 
+        document.getElementById("mySubmit").disabled = false;
+    }
+}
+/*
+  Exercise 05
+  -----------
+When the user clicks the ‘Register’ button, a message should be displayed informing them of a successful user registration.
+*/
+function submission(){
+    window.alert("User Registration Successful !")
+}
